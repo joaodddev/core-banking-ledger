d5e50b06a8bf043af08d5d08073362eea4c0062c
João Victor <joaodddev@gmail.com>
2026-06-18 11:09:40 -0300

    chore: replace application.yml

---
+++ src/main/java/br/com/joaodddev/banking/CoreBankingLedgerApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaodddev.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankingLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBankingLedgerApplication.class, args);
+	}
+
+}
